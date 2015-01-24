674df7f446f755883a231e7dd7014d1ba85db366
aditya <dev@chimeracoder.net>
2015-01-24 11:58:05 -0500

    Add Card struct (autogenerated using gojson)

---
+++ api.go
@@ -0,0 +1,4 @@
+package godeckbrew
+
+
+//go:generate gojson -o card.go -input json/mtg/card.json -name "Card"
---
+++ card.go
@@ -0,0 +1,37 @@
+package main
+
+type Card struct {
+	Cmc      int      `json:"cmc"`
+	Colors   []string `json:"colors"`
+	Cost     string   `json:"cost"`
+	Editions []struct {
+		Artist       string `json:"artist"`
+		Flavor       string `json:"flavor"`
+		ImageURL     string `json:"image_url"`
+		Layout       string `json:"layout"`
+		MultiverseID int    `json:"multiverse_id"`
+		Number       string `json:"number"`
+		Price        struct {
+			High   int `json:"high"`
+			Low    int `json:"low"`
+			Median int `json:"median"`
+		} `json:"price"`
+		Rarity   string `json:"rarity"`
+		Set      string `json:"set"`
+		SetID    string `json:"set_id"`
+		SetURL   string `json:"set_url"`
+		StoreURL string `json:"store_url"`
+		URL      string `json:"url"`
+	} `json:"editions"`
+	Formats struct {
+		Commander string `json:"commander"`
+		Legacy    string `json:"legacy"`
+		Vintage   string `json:"vintage"`
+	} `json:"formats"`
+	ID       string   `json:"id"`
+	Name     string   `json:"name"`
+	StoreURL string   `json:"store_url"`
+	Text     string   `json:"text"`
+	Types    []string `json:"types"`
+	URL      string   `json:"url"`
+}
